70be2e01a5b64209dccbea0beade802a92d81969
NPrivate <132364279+NPrivate@users.noreply.github.com>
2025-04-20 23:29:04 +0100

    Importing components

---
+++ reactjs-todolist/src/components/ToddoCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToddoCard = () => {
+  return (
+    <div>ToddoCard</div>
+  )
+}
+
+export default ToddoCard
---
+++ reactjs-todolist/src/components/TodoInput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const todoInput = () => {
+  return (
+    <div>todoInput</div>
+  )
+}
+
+export default todoInput
